b9cd71e0795ac06e9bbe1ef91dd553da125abd91
SylarK <syst.lucasamado@gmail.com>
2020-07-18 18:20:14 -0300

    Working in the confirm_reservation page logic.

--- restaurant/barbecue/views.py
+++ restaurant/barbecue/views.py
@@ -15,6 +15,19 @@ def menu(request):
     return render(request, "barbecue/menu.html")
 
 def reservation(request):
+
+    #open a default page
+    if(request.method == 'GET'):
+        return render(request, "barbecue/reservation.html")
+    else:
+        time_reserve = request.POST['time_reserve']
+        date_reserve = request.POST['date_reserve']    
+
+        return render(request, "barbecue/confirm.html", {
+
+            'time_reserve':time_reserve,
+            'date_reserve':date_reserve
+
+        })
     
-    return render(request, "barbecue/reservation.html")
 
